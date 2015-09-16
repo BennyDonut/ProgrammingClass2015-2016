d967fa1130f336c174eb33a435ff387dc1b96d11
BennyDonut <bchen@chicagotechacademy.org>
2015-09-16 15:21:51 -0500

    Update assignment4.rb

--- assignment4.rb
+++ assignment4.rb
@@ -1,6 +1,11 @@
-puts"how many hours do you work"
-hour=gets.chomp.to_i
-puts"how much do you make per hour"
-hour=gets.chomp.to_i
-puts"how many weeks did you work"
-week=gets.chomp.to_i
+puts "how many hours do you work"
+hours = gets.chomp.to_i
+
+puts "how much do you make per hour"
+wage = gets.chomp.to_i
+
+puts "how many weeks did you work"
+weeks = gets.chomp.to_i
+
+total = hours * wage * weeks
+puts "You would make #{total}."
