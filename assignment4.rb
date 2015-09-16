ac5cb1cb39fa79e00c754fb035b464f5bb218644
BennyDonut <bchen@chicagotechacademy.org>
2015-09-16 15:03:17 -0500

    Update assignment4.rb

--- assignment4.rb
+++ assignment4.rb
@@ -1,9 +1,5 @@
-how many hours/week do you work
-how much hours do you make
-how many weeks do you work
-
-puts"how many hours/week do you work"
-hours/week=gets.chomp.to_i
+puts"how many hours do you work"
+hour=gets.chomp.to_i
 puts"how much hours do you make"
 
 puts"how many weeks do you work"
